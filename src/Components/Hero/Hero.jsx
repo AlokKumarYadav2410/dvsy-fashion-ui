5929fecbc3f1456936d0028aa1fd36006ded5090
SubastralAlok <alokkumaryadav241004@gmail.com>
2025-12-26 19:18:06 +0530

    Hero and stats section completed with responsiveness

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import heroImg from '../../assets/hero.jpg'
+import model from '../../assets/model.avif'
 
 const Hero = () => {
   return (
     <div className='w-full h-[50vh] sm:h-[85vh] relative select-none rounded-lg overflow-hidden'>
       <img
-        src={heroImg}
+        src={model}
         alt="error"
         className="rounded-lg w-full h-full brightness-75 object-cover sm:object-fill"
       />
-      <div className="absolute top-[40%] sm:top-[45%] left-[5%] ">
-        <h1 className="text-2xl sm:text-6xl lg:text-7xl xl:text-8xl text-(--pri-text) font-semibold sm:font-bold tracking-widest font-[syne]">
+      <div className="absolute top-[40%] sm:top-[10%] left-[2%] ">
+        <h1 className="text-2xl sm:text-6xl lg:text-7xl xl:text-8xl text-(--pri-text) font-semibold sm:font-bold font-[syne]">
           DESIGN <br /> &nbsp; &nbsp; <span className='font-[manrope]'>& FREEDOM</span>
         </h1>
 
-        <p className="text-xs sm:text-sm md:text-lg text-(--sec-text) mx-10 sm:mx-38 lg:mt-6 capitalize w-[60vw] sm:w-[40vw] font-[manrope]">
+        <p className="text-xs sm:text-sm md:text-lg text-shadow-black text-shadow-2xs text-(--pri-text) mx-10 sm:mx-24 capitalize w-[60vw] sm:w-[30vw] font-[manrope]">
           Eplore independent style by embracing uniqueness with our
           exclusive designer apperal
         </p>
--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Stats = () => {
   return (
-    <div className='w-full grid grid-cols-1 text-center place-items-center sm:text-left sm:grid-cols-4 gap-4 my-10'>
+    <div className='w-full grid grid-cols-1 text-center place-items-center sm:text-left sm:grid-cols-4 gap-4 my-2 sm:my-10'>
         <div className="flex flex-col">
             <p className='text-sm sm:text-lg lg:text-xl font-semibold text-(--sec-text) font-[manrope]'>Designers</p>
             <h1 className='font-semibold text-3xl lg:text-5xl font-[manrope]'>150+</h1>
